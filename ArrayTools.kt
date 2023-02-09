e40f119bb1d15e0f099244f99ff80b1a9ccb39c1
foramjpatel5778@gmail.com <foramjpatel5778@gmail.com>
2023-02-09 18:37:17 -0500

    Done error handling while asking inout from the user and for that created separate functions.

--- ArrayTools.kt
+++ ArrayTools.kt
@@ -7,14 +7,48 @@
 fun main() {
     /* ---------------Perform Encryption--------------- */
     // Get String from user
-    print("Enter any string that you want to encrypt: ")
-    val plainText = readLine()
+    val plainText = checkStringUserInput("Enter any string that you want to encrypt: ")
     // Get shift value from user
-    print("Enter the shift value for encryption: ")
-    val shiftValue = readLine()!!.toInt()
+    val shiftValue = checkIntUserInput("Enter the shift value for encryption: ")
     // Get cipher text of given plain text and shift key using encrypt function.
     val encryptedString = encrypt(plainText, shiftValue)
     println("Encrypted string: $encryptedString")
+}
+
+// Check entered value is integer or not
+fun checkIntUserInput(message: String): Int {
+    var userInput = 0
+    var isValid = false
+    while(!isValid){
+        print(message)
+        val stringInput = readLine()
+        try {
+            userInput = stringInput!!.toInt()
+            isValid = true
+        }
+        catch(e: Exception) {
+            println("Please enter valid shift value.\n")
+        }
+    }
+
+    return userInput
+}
+
+// Check entered value contains only alphabets or not
+fun checkStringUserInput(message: String): String {
+    var stringInput = ""
+    var isValid = false
+    while(!isValid){
+        print(message)
+        stringInput = readLine()!!
+        if(!stringInput.matches("^[0-9]*$".toRegex())){
+            isValid = true
+        }else{
+            println("Please enter valid string value.\n")
+        }
+    }
+
+    return stringInput
 }
 
 // This function takes plain text and shift key and return cipher text by performing caesar cipher
