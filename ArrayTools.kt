00af35ccff87f701df20dc2104e8f277148a817c
foramjpatel5778@gmail.com <foramjpatel5778@gmail.com>
2023-02-09 18:20:48 -0500

    Get values from the user for caesar cipher encryption and create function called encryption which will return cipher text.

--- ArrayTools.kt
+++ ArrayTools.kt
@@ -5,5 +5,29 @@
                          each function. */
 
 fun main() {
-    
+    /* ---------------Perform Encryption--------------- */
+    // Get String from user
+    print("Enter any string that you want to encrypt: ")
+    val plainText = readLine()
+    // Get shift value from user
+    print("Enter the shift value for encryption: ")
+    val shiftValue = readLine()!!.toInt()
+    // Get cipher text of given plain text and shift key using encrypt function.
+    val encryptedString = encrypt(plainText, shiftValue)
+    println("Encrypted string: $encryptedString")
+}
+
+// This function takes plain text and shift key and return cipher text by performing caesar cipher
+fun encrypt(plainText: String, shiftValue: Int): String {
+    var cipherText = ""
+    for (i in 0 until plainText.length) {
+        if(plainText[i].isUpperCase()){
+            val character = (plainText[i].code + shiftValue - 65) % 26 + 65
+            cipherText += character.toChar()
+        }else{
+            val character = (plainText[i].code + shiftValue - 97) % 97 + 97
+            cipherText += character.toChar()
+        }
+    }
+    return cipherText
 }
