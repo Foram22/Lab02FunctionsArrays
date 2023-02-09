b53871608aa5bcbfdcb0861476a4ec161d91cef1
foramjpatel5778@gmail.com <foramjpatel5778@gmail.com>
2023-02-09 18:12:05 -0500

    Name, Student id and program description added at the top of the file.

--- ArrayTools.kt
+++ ArrayTools.kt
@@ -1,1 +1,9 @@
+// Foramben Jayeshkumar Patel
+// Student ID:-  A00272251
+/*  Program Description: This is a small kotlin command line application that has different function such as encryption (using Caesar Cipher), 
+                         arrayAvg(average of elements), arrayContains(search value) and reverse(gives reversed array). Also, prints results of 
+                         each function. */
 
+fun main() {
+    
+}
